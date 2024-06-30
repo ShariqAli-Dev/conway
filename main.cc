fe667e03d6eebf7d63e02e4aa1a7a4df7103b70e
ShariqAli-Dev <shariqali.dev@gmail.com>
2024-06-30 01:32:14 -0400

    counting local alive cells

--- main.cc
+++ main.cc
@@ -1,11 +1,13 @@
 #include "raylib.h"
+#include <stdio.h>
 
 static const unsigned int screenWidth = 800;
 static const unsigned int screenHeight = 600;
-static const unsigned int cellSize = 40;
+static const unsigned int cellSize = 200;
 static const unsigned int gridWidth = screenWidth / cellSize;
 static const unsigned int gridHeight = screenHeight / cellSize;
 static bool gridState[gridWidth][gridHeight] = {false};
+static bool pause = false;
 
 static void UpdateDraw(void);
 static void DrawGame(void);
@@ -26,6 +28,35 @@ int main()
     return 0;
 }
 
+int GetCellNeighborsAlive(int xCell, int yCell)
+{
+    printf("%d xCell, %d yCell\n");
+    int aliveNeighbors = 0;
+
+    // create a virtual 3x3 grid, I am the center
+    for (int xCord = -1; xCord < 2; xCord++)
+    {
+        for (int yCord = -1; yCord < 2; yCord++)
+        {
+            // im at the center, continue
+            if (xCord == 0 && yCord == 0)
+                continue;
+
+            int xCordNeighbor = xCord + xCell;
+            int yCordNeighbor = yCord + yCell;
+
+            bool neighborXInBoundry = xCordNeighbor >= 0 && xCordNeighbor < gridWidth;
+            bool neighborYInBoundry = yCordNeighbor < gridHeight && yCordNeighbor >= 0;
+            if (neighborXInBoundry && neighborYInBoundry)
+            {
+                if (gridState[xCordNeighbor][yCordNeighbor])
+                    aliveNeighbors++;
+            }
+        }
+    }
+    return aliveNeighbors;
+};
+
 void UpdateGame(void)
 {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
@@ -34,6 +65,29 @@ void UpdateGame(void)
         int xCell = (int)(mousePosition.x / cellSize);
         int yCell = (int)(mousePosition.y / cellSize);
         gridState[xCell][yCell] = !gridState[xCell][yCell];
+    }
+    if (IsKeyPressed('P'))
+    {
+        pause = !pause;
+    }
+    if (!pause)
+    {
+        // cell logic
+        for (int x = 0; x < screenWidth; x += cellSize)
+        {
+            for (int y = 0; y < screenHeight; y += cellSize)
+            {
+                int xCell = x / cellSize;
+                int yCell = y / cellSize;
+
+                if (gridState[xCell][yCell])
+                {
+                    int aliveNeighbors = GetCellNeighborsAlive(xCell, yCell);
+                    printf("alive neighbors %d\n", aliveNeighbors);
+                    gridState[xCell][yCell] = false;
+                }
+            }
+        }
     }
 }
 
@@ -61,10 +115,19 @@ void DrawGrid(void)
 
 void DrawGame(void)
 {
-
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawGrid();
+
+    if (pause)
+    {
+        DrawText("[P] to Unpause, go edit cells", 10, 10, 30, GREEN);
+    }
+    else
+    {
+        DrawText("[P] to Pause", 10, 10, 30, GREEN);
+    }
+
     DrawFPS(GetScreenWidth() - 95, 10);
     EndDrawing();
 }
