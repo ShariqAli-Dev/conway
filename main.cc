9542407751948c1b0d6b3275ffc126c11efaf84a
ShariqAli-Dev <shariqali.dev@gmail.com>
2024-06-30 02:13:56 -0400

    bugfixed

--- main.cc
+++ main.cc
@@ -1,5 +1,4 @@
 #include "raylib.h"
-#include <stdio.h>
 
 static const unsigned int screenWidth = 800;
 static const unsigned int screenHeight = 600;
@@ -17,7 +16,7 @@ static void UpdateGame(void);
 int main()
 {
     InitWindow(screenWidth, screenHeight, "Conway's Game of Life");
-    SetTargetFPS(10);
+    SetTargetFPS(5);
 
     while (!WindowShouldClose())
     {
@@ -31,8 +30,8 @@ int main()
 void CellGetAliveAndDeadNeighbors(int xCell, int yCell, int *aliveNeighbors, int *deadNeighbors)
 {
     // create a virtual 3x3 grid, I am the center
-    *aliveNeighbors = 0;
-    *deadNeighbors = 0;
+    // *aliveNeighbors = 0;
+    // *deadNeighbors = 0;
 
     for (int xCord = -1; xCord < 2; xCord++)
     {
@@ -79,7 +78,7 @@ void UpdateGame(void)
     {
         bool newGridState[gridWidth][gridHeight] = {false};
         // cell logic
-        for (int x = 0; x < gridWidth; x += x++)
+        for (int x = 0; x < gridWidth; x++)
         {
             for (int y = 0; y < gridHeight; y++)
             {
@@ -87,7 +86,6 @@ void UpdateGame(void)
                 int aliveNeighbors = 0;
                 int deadNeighbors = 0;
                 CellGetAliveAndDeadNeighbors(x, y, &aliveNeighbors, &deadNeighbors);
-                printf("aliveN: %d, deadN, %d\n", aliveNeighbors, deadNeighbors);
                 if (cellIsAlive)
                 {
                     if (aliveNeighbors < 2 || aliveNeighbors > 3)
@@ -103,7 +101,7 @@ void UpdateGame(void)
                 {
                     // neighbors == 3, live
                     if (aliveNeighbors == 3)
-                        gridState[x][y] = true;
+                        newGridState[x][y] = true;
                 }
             }
         }
